11d7486a1a6532c5b75ab90f46d1662f70418219
killinsun <noneck.0311@gmail.com>
2017-11-27 08:02:58 +0000

    a

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,7 +1,22 @@
 var socket = io();
 
 $(function(){
-	$('form').submit(function(){
+	$('#form_join').submit(function(){
+
+		var user_name = $('#user_name').val()
+
+		$('#join_view').hide();
+		$('#chat_view').show();
+
+		return false;
+	});
+
+	socket.on('message', function(msg, id){
+		$('#message').append($('<li>').text(id + " : " + msg));
+
+	});
+
+	$('#form_chat').submit(function(){
 
 		var msg = $('#msg').val()
 
--- routes/chat.js
+++ routes/chat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('chat_dev', { title: 'OSS-PLAYON.JP' });
+	res.render('chat', { title: 'OSS-PLAYON.JP' });
 });
 
 module.exports = router;
