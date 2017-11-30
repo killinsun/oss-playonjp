140b58b5f5c2d19c9919de8c189b62da6f280b37
killinsun <noneck.0311@gmail.com>
2017-11-30 08:02:50 +0000

    Create new mongdb schemas.(user,ignorelist)

--- model.js
+++ model.js
@@ -2,11 +2,30 @@ let mongoose = require('mongoose');
 let db		 = mongoose.connect('mongodb://localhost/oss-playonjp');
 
 let Chat = new mongoose.Schema({
-	room_name:	String,
-	date_time:	String,
-	socket_id:	String,
-	user_name:	String,
+	room:		String,
+	sc_id:		String,
+	usr_id:		String,
+	usr_name:	String,
 	msg		 :	String
+	say_date:	String,
 });
 
+let User = new mongoose.Schema({
+	usr_id:		String,
+	pwd:		String,
+	in_msg:		String,
+	ot_msg:		String,
+	font_size:	Number,
+	font_color:	String,
+	cr_date:	String
+});
+
+let IgnoreList = mongoose.Schema({
+	usr_id:		String,
+	ignore:		String
+});
+
+
 exports.Chat = db.model('Chat', Chat);
+exports.User = db.model('User', User);
+exports.IgnoreList = db.model('ignoreList', IgnoreList);
